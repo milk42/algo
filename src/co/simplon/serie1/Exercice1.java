86c2c30ab9feedfd9477e6c112147c616aaaeaf1
milk42 <laetitia.fouchere.lp2@gmail.com>
2017-10-12 15:36:51 +0200

    ce que je veux

---
+++ src/co/simplon/serie1/Exercice1.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package co.simplon.serie1;
+
+/**
+ * @author Utilisateur
+ *ceci est un exo
+ */
+public class Exercice1 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		System.out.println("Hello world !");
+		
+	}
+
+}
